1fa0b87a23625517481bdad8447d1341f2af43ee
deeenes <turei.denes@gmail.com>
2022-06-22 00:13:42 +0200

    set up project template

---
+++ docs/src/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+from datetime import datetime
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import sys
+import pathlib
+
+here = pathlib.Path(__file__).parent
+sys.path.insert(0, str(here.parent))
+
+import project_name  # noqa: E402
+
+# -- Project information -----------------------------------------------------
+
+project = 'project_name'
+version = project_name.__version__
+author = ', '.join(project_name.__author__)
+years = '-'.join(sorted({'2022', f'{datetime.now():%Y}'}))
+copyright = f'{years}, Saez Lab'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',  # not for output but to remove warnings
+    'sphinx_rtd_theme',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Autodoc configuration ---------------------------------------------------
+
+autodoc_mock_imports = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 2,
+    'collapse_navigation': True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
---
+++ project_name/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2022, Saez Lab
+#
+# File author(s): Denes Turei <turei.denes@gmail.com>
+#                 ...
+#
+# Distributed under GPLv3 license, see the file `LICENSE`.
+#
+
+
+"""
+Python project template.
+"""
+
+__all__ = ['twentythree']
+
+from ._metadata import __author__, __version__
+
+
+def twentythree() -> int:
+    """
+    The number twenty-three.
+    """
+
+    return 23
