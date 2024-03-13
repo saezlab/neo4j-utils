24b2a1bea31d8d6bea79e82143786bb8e06026cb
Nils Krehl <nils.krehl@posteo.de>
2024-03-13 15:30:48 +0100

    #6: get neo4j version with regex

--- neo4j_utils/_n4jversion.py
+++ neo4j_utils/_n4jversion.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 __all__ = ['Neo4jVersion']
 
@@ -13,8 +14,13 @@ class Neo4jVersion:
     def __init__(self):
         try:
             cmd = ['neo4j-admin', '--version']
-            version = subprocess.check_output(cmd).decode().strip()
+            output = subprocess.check_output(cmd).decode().strip()
+            semver_pattern = r'\b\d+\.\d+\.\d+\b'
+            matches = re.findall(semver_pattern, output)
+            version = matches[0]
             # Output format: "X.Y.Z" or "X.Y.Z-SNAPSHOT"
+            if version == None:
+                raise ValueError(f"No neo4j version found in {output}")
             self.major = int(version.split('.')[0])
         except (subprocess.CalledProcessError, ValueError, IndexError) as e:
             print(f'Error detecting Neo4j version: {e}')
